27ee2cd5a99fa869d9f435f35c41c162e3a5ac6b
IPconfig <Justin.haan92@gmail.com>
2016-08-08 17:14:24 +0200

    Revert "Cleanup; remove duplicates in the "Open with" menu"
    This reverts commit 82e1b8fa4e8d67cde0711d2b6964c7f4c6b63730.

--- scripts/05_cleanup.sh
+++ scripts/05_cleanup.sh
@@ -1,8 +1,3 @@
-cleanup_open_with {
-  # Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
-  /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
-}
-
 cleanup_brew() {
   brew cleanup --force
   rm -rf "$(brew --cache)"
