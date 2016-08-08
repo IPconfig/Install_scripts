698578e020d7bed1cc4119c2161e6461e9800b31
IPconfig <Justin.haan92@gmail.com>
2016-08-08 15:47:37 +0200

    remove comments
    TODO: fix python so it will install in a virtualenv

--- scripts/01_environments.sh
+++ scripts/01_environments.sh
@@ -8,22 +8,3 @@ install_python() {
   # install some eggs
   pip3 install livestreamer subliminal
 }
-
-#install_ruby() {
-#  brew reinstall chruby ruby-install
-#  ruby-install --src-dir "$(mktemp -d)" --latest ruby
-#  source '/usr/local/share/chruby/chruby.sh'
-#  chruby ruby
-  # install some gems
-#  gem install --no-document bundler chromedriver2-helper maid pry redcarpet rubocop seeing_is_believing site_validator video_transcoding watir-webdriver
-#}
-
-#install_node() {
-#  brew reinstall nvm
-#  export NVM_DIR="${HOME}/.nvm"
-#  source "$(brew --prefix nvm)/nvm.sh"
-#  nvm install node
-#  nvm alias default node
-  # install some packages
-#  npm install --global eslint eslint-plugin-immutable eslint-plugin-shopify how2 jsonlint nativefier nightmare npm-check-updates pageres-cli watch webcoach
-#}
