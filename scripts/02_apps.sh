783f6bb25cf83ccd00c8680fc0263da9b03b4cf8
Justin de Haan <justin.haan92@gmail.com>
2016-08-08 23:40:45 +0200

    check if homebrew formulas can be installed/upgraded

--- scripts/02_apps.sh
+++ scripts/02_apps.sh
@@ -1,10 +1,34 @@
+#Helper functions
+brew_install_or_upgrade() {
+  if brew_is_installed "$1"; then
+    if brew_is_upgradable "$1"; then
+      echo "Upgrading %s ..." "$1"
+      brew upgrade "$@"
+    else
+      echo "Already using the latest version of %s. Skipping ..." "$1"
+    fi
+  else
+    echo "Installing %s ..." "$1"
+    brew install "$@"
+  fi
+}
+
+brew_is_installed() {
+  brew list -1 | grep -Fqx "$1"
+}
+
+brew_is_upgradable() {
+  ! brew outdated --quiet "$1" >/dev/null
+}
+#-----------------
+
 install_brew_apps() {
-  brew install asciinema asciinema2gif #install terminal screen recorder
-  brew install exiftool #R/W exif information
-  brew install ghi git git-extras #git from the commandline
-  brew install gifify #make gifs from movies
-  brew install hr #horizontal ruler for the terminal
-  brew install duti #needer later on to set default app for extensions
+  brew_install_or_upgrade asciinema asciinema2gif #install terminal screen recorder
+  brew_install_or_upgrade exiftool #R/W exif information
+  brew_install_or_upgrade ghi git git-extras #git from the commandline
+  brew_install_or_upgrade gifify #make gifs from movies
+  brew_install_or_upgrade hr #horizontal ruler for the terminal
+  brew_install_or_upgrade duti #needer later on to set default app for extensions
 
   # install and configure tor
   #brew install tor torsocks
