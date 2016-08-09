f915e7c76b905bd37b4f9bd8d6932d3680be29ea
Justin de Haan <justin.haan92@gmail.com>
2016-08-09 16:26:37 +0200

    delete torbrowser, drop-to-gif, wwdc and screenflow
    replaced pycharm-ce to versions tap and suspicious packages to brew
    cask apps since it's not a qlplugin anymore

--- scripts/02_apps.sh
+++ scripts/02_apps.sh
@@ -6,25 +6,21 @@ install_brew_apps() {
   brew install hr #horizontal ruler for the terminal
   brew install duti #needer later on to set default app for extensions
 
-  # install and configure tor
-  #brew install tor torsocks
-  #cp "$(brew --prefix)/etc/tor/torrc.sample" "$(brew --prefix)/etc/tor/torrc"
-  #echo 'ExitNodes {us}' >> "$(brew --prefix)/etc/tor/torrc"
 }
 
 install_cask_apps() {
-  brew cask install --appdir='/Applications' cyberduck docker-toolbox drop-to-gif dropbox flux github-desktop handbrake malwarebytes-anti-malware microsoft-office onyx postman pycharm-ce sequel-pro screenflow shotcut steam torbrowser transmission visual-studio-code whatsapp xact wwdc
+  brew cask install --appdir='/Applications' cyberduck docker-toolbox dropbox flux github-desktop handbrake malwarebytes-anti-malware microsoft-office onyx postman sequel-pro shotcut steam suspicious-package transmission visual-studio-code whatsapp xact
 
   # install alternative versions
   brew tap caskroom/versions
-  brew cask install --appdir='/Applications' firefox-beta google-chrome-canary royal-tsx-beta
+  brew cask install --appdir='/Applications' firefox-beta google-chrome-canary pycharm-ce royal-tsx-beta
 
   # prefpanes, qlplugins, colorpickers
-  brew cask install betterzipql colorpicker-skalacolor epubquicklook qlcolorcode qlplayground qlstephen quicklook-json ttscoff-mmd-quicklook qlmarkdown qlprettypatch quicklook-csv webpquicklook suspicious-package
+  brew cask install betterzipql colorpicker-skalacolor epubquicklook qlcolorcode qlplayground qlstephen quicklook-json ttscoff-mmd-quicklook qlmarkdown qlprettypatch quicklook-csv webpquicklook
 }
 
 install_mas_apps() {
-  readonly local mas_apps=('1password=443987910' 'xcode=497799835' 'WiFi Scanner=411680127' 'The Unarchiver=425424353' 'Haskell=841285201')
+  readonly local mas_apps=('1password=443987910' 'xcode=497799835' 'WiFi Scanner=411680127' 'The Unarchiver=425424353' 'Haskell for Mac=841285201')
 
   mas signin "${mas_email}" "${mas_password}"
 
