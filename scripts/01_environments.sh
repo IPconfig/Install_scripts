79a986d973c915d72851e46b9d8a7b597ce832f9
Justin de Haan <justin.haan92@gmail.com>
2016-09-05 15:59:56 +0200

    Add completion, default editor and history to  .bash_profile

--- scripts/01_environments.sh
+++ scripts/01_environments.sh
@@ -14,6 +14,40 @@ install_bash() {
   echo "Adding the newly installed shell to the list of allowed shells"
   sudo -S sh -c 'echo "/usr/local/bin/bash" >> /etc/shells' <<< "${sudo_password}" 2> /dev/null
   sudo -S chsh -s '/usr/local/bin/bash' "${USER}" <<< "${sudo_password}" 2> /dev/null
+
+  # Configure Bash_History
+  if grep -q "HISTSIZE=100000 " ~/.bash_profile
+  then
+    echo "Existing bash_history found in bash_profile, nothing added"
+  else
+    configure_bash
+  fi
+
+  configure_bash() {
+    {
+      # Bash completion
+      echo "if [ -f /etc/bash_completion ] && ! shopt -oq posix; then"
+      echo "  . /etc/bash_completion"
+      echo"fi"
+
+      # Use Nano as default texteditor since I hate vim (Sorry)  
+      echo "export EDITOR=/usr/bin/nano"
+      echo "" 
+
+      # timestamps for later analysis. www.debian-administration.org/users/rossen/weblog/1
+      echo "export HISTTIMEFORMAT='%F %T '"
+      # keep history up to date, across sessions, in realtime
+      #  http://unix.stackexchange.com/a/48113
+      echo "export HISTCONTROL=ignoredups:erasedups         # no duplicate entries"
+      echo "export HISTSIZE=100000                          # big big history (default is 500)"
+      echo "export HISTFILESIZE=$HISTSIZE                   # big big history"
+      echo "which shopt > /dev/null && shopt -s histappend  # append to history, don't overwrite it"
+
+      echo "# Save and reload the history after each command finishes"
+      echo "export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND""
+      echo "# ^ the only downside with this is [up] on the readline will go over all history not just this bash session."
+    }
+  } >> ~/.bash_profile
 }
 
 install_python() {
