dd76312b1f1beb44e165d973a169d25a9c3511e7
Justin de Haan <justin.haan92@gmail.com>
2018-01-17 16:49:32 +0100

    + pipenv; - virtualenv/virtualfish

--- scripts/01_environments.sh
+++ scripts/01_environments.sh
@@ -9,26 +9,16 @@ install_fish(){
   brew install fish --HEAD
   sudo --stdin sh -c 'echo "/usr/local/bin/fish" >> /etc/shells' <<< "${sudo_password}" 2> /dev/null
   sudo --stdin chsh -s '/usr/local/bin/fish' "${USER}" <<< "${sudo_password}" 2> /dev/null
-  sudo --stdin sh -c 'echo "eval (python3 -m virtualfish compat_aliases auto_activation projects)" >> ~/.config/fish/config.fish'
 }
 
 install_python() {
   brew install python # this will already prefer the brewed one above the system installed one
-  pip install --upgrade pip
+  pip install --upgrade pip setuptools wheel
 
   # python3
   brew install python3
   pip3 install --upgrade pip setuptools wheel
-  pip3 install --force-reinstall --upgrade pip # point 'pip' to python3 instead of python2
-  pip3 install virtualfish
-
-  # create virtualenvironments 
-  export WORKON_HOME=~/.virtualenvs
-  mkdir -p $WORKON_HOME
-  source /usr/local/bin/virtualenvwrapper.sh
-
-  # install pipsi to install scripts into seperate virtualenvs to shield them from your system and each other
-  curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
+  pip3 install pipenv # harnesses Pipfile, pip, and virtualenv into one single command.
 }
 
 install_ruby() {
