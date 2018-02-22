d712afb5ac388c6507bf812c2d7e0f951791f460
IPconfig <justin.haan92@gmail.com>
2018-02-22 21:30:14 +0100

    - unknown command for Disk Utility preferences

--- scripts/04_preferences.sh
+++ scripts/04_preferences.sh
@@ -202,7 +202,6 @@ osascript -e 'tell application "System Preferences" to quit'
   # Enable the debug menu in Disk Utility
   defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
   defaults write com.apple.DiskUtility advanced-image-options -bool true
-  CFPreferencesAppSynchronize "com.apple.DiskUtility"
 
   ###############################################################################
   # Terminal                                                                    #
